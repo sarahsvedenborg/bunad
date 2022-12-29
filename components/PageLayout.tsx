475f83827f550c24351d2d2cc388eb915ce45505
Sarah Svedenborg <sarah.svedenborg@gmail.com>
2022-12-29 23:02:03 +0100

    initial commit

---
+++ components/PageLayout.tsx
@@ -0,0 +1,13 @@
+export const PageLayout = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>item</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default PageLayout;
